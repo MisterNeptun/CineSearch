ea40200e928941ed197c26ea4e39538806f71a4c
Nikolaj Veljkovic <nikolaj.veljkovic@mygymer.ch>
2024-05-29 15:55:13 +0200

    Added comments

--- testing/webapp_test.py
+++ testing/webapp_test.py
@@ -1,7 +1,8 @@
 from bottle import request, route, run, static_file, template
-import mysql.connector
+from mysql.connector import connect
 
-def connect():
+# Funktion zur Erstellung von der DatenBank
+def connectDB():
     mydb = mysql.connector.connect(
       host="web3.kinet.ch",    
       user="omdb_user",
@@ -10,32 +11,39 @@ def connect():
     )
     return mydb
 
+# ERKLÄRUNG?
 @route('/')
 def index():
     return template("../views/startseite.html", title="Startseite")
 
+# Routing der about page
 @route("/about")
 def about():
     page = "Wir haben diese App im EF-Informatik erstellt!"
     return page
 
+# ISt das nötig?
+"""
+# Rückgabe der Id-Webseite
 @route("/serie/<id>")
 def film(id):
     return "Du hast Details zur Serie mit der id " + id + " verlangt"
+"""
+
+
 @route('/movie')
 def movie():
 
-    mydb = connect()
+    mydb = connectDB()
     mycursor = mydb.cursor(named_tuple=True)    
-    mycursor.execute("SELECT * FROM movies WHERE name LIKE '%Titanic%'")
+    mycursor.execute("SELECT * FROM movies WHERE name LIKE id=ZAHL")
 
     myresult = mycursor.fetchone()
     
     mydb.close()
+    
     print(myresult)
     return template("movie.html", movie=myresult)
-    
-
 
 @route("/search")
 def search():
